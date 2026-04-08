4b7c5c07578d05839c552c9042191cebe0dcb885
Fernando94654 <A00841856@tec.mx>
2026-04-07 23:20:35 -0600

    use deploy backend in fronted

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://otrix-dev.up.railway.app';
 
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('loginForm');
